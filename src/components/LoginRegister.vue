f1644b83c296b11f9dfb358fb3f13f0bfb987aeb
Clinton Bugtong <clintbugs.dev@gmail.com>
2020-10-19 00:32:38 +0800

    Update

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input
+      v-if="tab == 'register'"
+      v-model="formData.name"
+      class="q-mb-md"
+      outlined
+      label="Name"
+    />
+    <q-input
+      v-model="formData.email"
+      class="q-mb-md"
+      outlined
+      label="Email"
+      type="email"
+    />
+    <q-input
+      v-model="formData.password"
+      class="q-mb-md"
+      outlined
+      label="Password"
+      type="password"
+    />
+    <div class="row">
+      <q-space />
+      <q-btn color="primary" :label="tab" type="submit"/>
+    </div>
+  </q-form>
+</template>
+<script>
+export default {
+  props: ["tab"],
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+      submitForm(){
+          if (this.tab == 'login') {
+              console.log('login')
+          } else {
+              console.log('register')
+          }
+      }
+  }
+};
+</script>
--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,11 +1,45 @@
 <template>
-  <q-page class="flex">
-    <h5>Auth page</h5>
+  <q-page class="flex q-pa-md">
+    <q-card class="full-width">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <LoginRegister :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <LoginRegister :tab="tab" />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
+import LoginRegister from "components/LoginRegister";
 export default {
-  name: 'PageAuth'
-}
+  name: "PageAuth",
+  data() {
+    return {
+      tab: "login"
+    };
+  },
+  components: {
+    LoginRegister
+  }
+};
 </script>
